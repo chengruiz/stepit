a96b6f67c60d1d74845d5b6c6850b4ec59127100
doreaming <86215195+X1a0ming@users.noreply.github.com>
2026-02-09 14:49:28 +0800

    feat: implement observation history module (#2)
    * feat: add HistoryBuffer class for managing historical data in neuro policy
    
    * feat: update HistoryBuffer class to inherit from Module and register as a module
    
    * Update plugin/policy_neuro/src/history_buffer.cpp
    
    * Update plugin/policy_neuro/src/history_buffer.cpp
    
    * Update plugin/policy_neuro/src/history_buffer.cpp
    
    ---------
    
    Co-authored-by: Chengrui Zhu <jewel@zju.edu.cn>

---
+++ plugin/policy_neuro/include/stepit/policy_neuro/history_buffer.h
@@ -0,0 +1,34 @@
+#ifndef STEPIT_NEURO_POLICY_HISTORY_BUFFER_H_
+#define STEPIT_NEURO_POLICY_HISTORY_BUFFER_H_
+
+#include <deque>
+#include <stepit/policy_neuro/field.h>
+
+namespace stepit {
+namespace neuro_policy {
+class HistoryBuffer : public Module {
+ public:
+  HistoryBuffer(const PolicySpec &policy_spec, const std::string &home_dir);
+  void initFieldProperties() override;
+  bool reset() override;
+  bool update(const LowState &low_state, ControlRequests &requests, FieldMap &result) override;
+
+ private:
+  YAML::Node config_;
+  
+  struct BufferConfig {
+    FieldId source_id;
+    FieldId target_id;
+    std::uint32_t history_len;
+    std::uint32_t source_size;
+    bool newest_first;  // true: newest->oldest, false: oldest->newest
+    std::deque<VecXf> history;
+  };
+  
+  std::vector<BufferConfig> buffers_;
+  VecXf output_buffer_;
+};
+}  // namespace neuro_policy
+}  // namespace stepit
+
+#endif  // STEPIT_NEURO_POLICY_HISTORY_BUFFER_H_
