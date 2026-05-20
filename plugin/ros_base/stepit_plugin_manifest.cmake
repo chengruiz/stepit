e776d68900b2c40591144f7472c3fe5a886e4972
Chengrui Zhu <jewel@zju.edu.cn>
2026-05-20 11:04:00 +0800

    fix: update CMakeLists.txt and plugin manifest to improve catkin package handling

--- plugin/ros_base/stepit_plugin_manifest.cmake
+++ plugin/ros_base/stepit_plugin_manifest.cmake
@@ -1,7 +1,7 @@
 stepit_declare_plugin(NAME ros_base DEPENDS
     joystick_base
 )
-find_package(catkin QUIET COMPONENTS roscpp sensor_msgs std_msgs stepit_ros_msgs)
+find_package(catkin QUIET COMPONENTS roscpp sensor_msgs std_msgs)
 if (NOT catkin_FOUND)
   stepit_plugin_mark_unbuildable("Missing catkin.")
   return()
